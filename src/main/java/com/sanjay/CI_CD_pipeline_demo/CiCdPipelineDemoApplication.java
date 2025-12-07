a495f973982d16a82bdf9eaec381c50a7fc05c7a
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2025-12-07 22:02:01 +0530

    modified src

--- src/main/java/com/sanjay/CI_CD_pipeline_demo/CiCdPipelineDemoApplication.java
+++ src/main/java/com/sanjay/CI_CD_pipeline_demo/CiCdPipelineDemoApplication.java
@@ -2,9 +2,17 @@ package com.sanjay.CI_CD_pipeline_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CiCdPipelineDemoApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from sanjay's Spring Boot !";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdPipelineDemoApplication.class, args);
