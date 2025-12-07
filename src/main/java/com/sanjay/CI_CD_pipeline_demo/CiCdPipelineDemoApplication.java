63de3ac19a39b3d0b332d40eaebf815015c63eb0
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2025-12-07 21:37:27 +0530

    first commit

---
+++ src/main/java/com/sanjay/CI_CD_pipeline_demo/CiCdPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.CI_CD_pipeline_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjay/CI_CD_pipeline_demo/CiCdPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.CI_CD_pipeline_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
